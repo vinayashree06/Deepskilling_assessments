be9ce476ec490d02429988b79dc04271751b6acd
vinayashree06 <vvinayashreer@gmail.com>
2025-06-27 20:21:21 +0530

    Add files via upload

---
+++ week2/Mockito/MyService.java
@@ -0,0 +1,12 @@
+public class MyService {
+
+    private ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+}
