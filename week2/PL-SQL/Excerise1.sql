db5dc234f0a83436c1b8556e9e2279ab886896de
vinayashree06 <vvinayashreer@gmail.com>
2025-06-27 17:17:07 +0530

    Updated Excerise1.sql

--- week2/PL-SQL/Excerise1.sql
+++ week2/PL-SQL/Excerise1.sql
@@ -9,7 +9,7 @@ INSERT INTO Customers VALUES (1, 'Vinaya', 65, 12000, 'FALSE');
 INSERT INTO Customers VALUES (2, 'Vidya', 55, 8000, 'FALSE');
 INSERT INTO Customers VALUES (3, 'Supriya', 70, 15000, 'FALSE');
 INSERT INTO Customers VALUES (4, 'Sindhu', 40, 5000, 'FALSE');
-
+INSERT INTO Customers VALUES (5, 'Varshu', 25, 6000, 'FALSE');
 COMMIT;
 
 CREATE TABLE Loans (
@@ -22,8 +22,8 @@ CREATE TABLE Loans (
 INSERT INTO Loans VALUES (101, 1, 10, SYSDATE + 10);  
 INSERT INTO Loans VALUES (102, 2, 12, SYSDATE + 40);  
 INSERT INTO Loans VALUES (103, 3, 9, SYSDATE + 20);   
-INSERT INTO Loans VALUES (104, 4, 11, SYSDATE + 5);   
-
+INSERT INTO Loans VALUES (104, 4, 11, SYSDATE + 5);  
+INSERT INTO Loans VALUES (105, 5, 13, SYSDATE + 10);
 COMMIT;
 --Scenario:1
 DECLARE
@@ -38,10 +38,8 @@ BEGIN
     UPDATE Loans
     SET InterestRate = InterestRate - 1
     WHERE LoanID = rec.LoanID;
-
     DBMS_OUTPUT.PUT_LINE('Discount applied for Loan ID: ' || rec.LoanID);
   END LOOP;
-
   COMMIT;
 END;
 /
@@ -51,22 +49,18 @@ SELECT * FROM Loans;
 DECLARE
   CURSOR cur_customers IS
     SELECT CustomerID, Balance FROM Customers;
-
 BEGIN
   FOR rec IN cur_customers LOOP
     IF rec.Balance > 10000 THEN
       UPDATE Customers
       SET IsVIP = 'TRUE'
       WHERE CustomerID = rec.CustomerID;
-
       DBMS_OUTPUT.PUT_LINE('Promoted to VIP: Customer ID ' || rec.CustomerID);
     END IF;
   END LOOP;
-
   COMMIT;
 END;
 /
-
 SELECT * FROM Customers;
 
 --Scenario:3
@@ -84,10 +78,4 @@ BEGIN
   END LOOP;
 END;
 /
-
 SELECT * FROM Loans;
-
-
-
-
-
