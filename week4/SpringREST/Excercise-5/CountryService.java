87bd3a375167c0ba80348184d20f4949cf7ce262
vinayashree06 <vvinayashreer@gmail.com>
2025-07-11 09:20:35 +0530

    Adding CountryService.java

---
+++ week4/SpringREST/Excercise-5/CountryService.java
@@ -0,0 +1,21 @@
+package com.cognizant.spring_learn.service;
+
+import com.cognizant.spring_learn.Country;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+    public Country getCountry(String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        List<Country> countries = context.getBean("countryList", List.class);
+
+        return countries.stream()
+                .filter(c -> c.getCode().equalsIgnoreCase(code))
+                .findFirst()
+                .orElse(null); // or throw custom exception
+    }
+}
