e944f037bc5b9b24b6472884c4ecc262c37dec7a
vinayashree06 <vvinayashreer@gmail.com>
2025-07-25 13:52:05 +0530

    Adding App.js

---
+++ week6/Excercise-3/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculateScore from './Components/CalculateScore';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculateScore name="Vinaya Shree R" school="B.G.S Public School" total={492} goal={5} />
+    </div>
+  );
+}
+
+export default App;
